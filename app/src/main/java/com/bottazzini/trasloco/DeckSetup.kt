42dfa615a7976d3c33cf025d3663d8f3aa16495c
Bruno Bottazzini <bottazzinibruno@gmail.com>
2021-07-27 19:04:00 +0200

    Implement first version

---
+++ app/src/main/java/com/bottazzini/trasloco/DeckSetup.kt
@@ -0,0 +1,36 @@
+package com.bottazzini.trasloco
+
+class DeckSetup {
+    companion object {
+        private var randomDeck = ArrayList<String>()
+        private var subDeckMap = HashMap<String, List<String>>()
+
+        private val SEMI = listOf<String>("b", "c", "d", "s")
+
+        private fun getOrderedDeck(): List<String> {
+            val orderedDeck = ArrayList<String>()
+            for (s in SEMI) {
+                for (i in 1..10) {
+                    orderedDeck.add(s + i)
+                }
+            }
+
+            return orderedDeck
+        }
+
+        fun getSubDeckMap() = subDeckMap
+
+        fun prepareSubDecks() {
+            subDeckMap["1"] = randomDeck.subList(0, 10)
+            subDeckMap["2"] = randomDeck.subList(10, 20)
+            subDeckMap["3"] = randomDeck.subList(20, 30)
+            subDeckMap["4"] = randomDeck.subList(30, 40)
+        }
+
+        fun shuffleDeck() {
+            val deck = (getOrderedDeck() as ArrayList)
+            deck.shuffle()
+            randomDeck = deck
+        }
+    }
+}
--- app/src/main/java/com/bottazzini/trasloco/GameActivity.kt
+++ app/src/main/java/com/bottazzini/trasloco/GameActivity.kt
@@ -1,37 +1,19 @@
 package com.bottazzini.trasloco
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.coroutines.EmptyCoroutineContext
+import com.bottazzini.trasloco.utils.CardNameTranslator
 
 class GameActivity : AppCompatActivity() {
-    companion object {
-        private val SEMI = listOf<String>("b", "c", "d", "s")
-    }
 
-    private var randomDeck: List<String> = getRandomDeck()
-    private var subDeckMap: HashMap<String, List<String>> = HashMap()
-
-    private fun getOrderedDeck(): List<String> {
-        val orderedDeck = ArrayList<String>()
-        for (s in SEMI) {
-            for (i in 1..10) {
-                orderedDeck.add(s + i)
-            }
-        }
-
-        return orderedDeck
-    }
-
-    private fun getRandomDeck(): List<String> {
-        val deck = (getOrderedDeck() as ArrayList)
-        deck.shuffle()
-        return deck
-    }
+    private var subDeckMap = HashMap<String, List<String>>()
+    private var cardTableMap = HashMap<String, ArrayList<String>>()
+    private var selectedCard: String? = null
+    private var selectedPositionId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,40 +22,162 @@ class GameActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        prepareGame()
+        DeckSetup.shuffleDeck()
+        DeckSetup.prepareSubDecks()
+        subDeckMap = DeckSetup.getSubDeckMap()
+
+        prepareTable()
     }
 
-    fun prepareGame() {
-        subDeckMap["1"] = randomDeck.subList(0, 9)
-        subDeckMap["2"] = randomDeck.subList(10, 19)
-        subDeckMap["3"] = randomDeck.subList(20, 29)
-        subDeckMap["4"] = randomDeck.subList(30, 39)
-
-        findViewById<ImageView>(R.id.subDeck11).setImageResource(getImageId(subDeckMap["1"]!![0]))
-        findViewById<ImageView>(R.id.subDeck12).setImageResource(getImageId(subDeckMap["1"]!![1]))
-        findViewById<ImageView>(R.id.subDeck13).setImageResource(getImageId(subDeckMap["1"]!![2]))
-
-        findViewById<ImageView>(R.id.subDeck21).setImageResource(getImageId(subDeckMap["2"]!![0]))
-        findViewById<ImageView>(R.id.subDeck22).setImageResource(getImageId(subDeckMap["2"]!![1]))
-        findViewById<ImageView>(R.id.subDeck23).setImageResource(getImageId(subDeckMap["2"]!![2]))
-
-        findViewById<ImageView>(R.id.subDeck31).setImageResource(getImageId(subDeckMap["3"]!![0]))
-        findViewById<ImageView>(R.id.subDeck32).setImageResource(getImageId(subDeckMap["3"]!![1]))
-        findViewById<ImageView>(R.id.subDeck33).setImageResource(getImageId(subDeckMap["3"]!![2]))
-
-        findViewById<ImageView>(R.id.subDeck41).setImageResource(getImageId(subDeckMap["4"]!![0]))
-        findViewById<ImageView>(R.id.subDeck42).setImageResource(getImageId(subDeckMap["4"]!![1]))
-        findViewById<ImageView>(R.id.subDeck43).setImageResource(getImageId(subDeckMap["4"]!![2]))
+    fun resetClick(view: View) {
+        if (selectedCard != null) {
+            selectedCard = null
+            selectedPositionId = null
+            val textView = findViewById<TextView>(R.id.selectedCardTextView)
+            textView.text = resources.getString(R.string.nessuna_carta_selezionata)
+        }
     }
 
     fun subDeckClick(view: View) {
-        val id = getImageId(randomDeck[0])
-
-        val imageView = findViewById<ImageView>(R.id.subDeck11)
-        imageView.setImageResource(id)
+        val cardPosition = view.id
+        val imageView = findViewById<ImageView>(cardPosition)
+        val line = (imageView.tag) as String
+        val subDeck = getSubDeckListConcurrentSafely(line)
+        if (subDeck.isNotEmpty()) {
+            dealCard(line)
+            if (getSubDeckListConcurrentSafely(line).isEmpty()) {
+                setImage(cardPosition, "zero")
+            }
+        }
     }
 
-    private fun getImageId(position: String) : Int {
-        return resources.getIdentifier("drawable/$position", "id", this.packageName)
+    fun gameCardClick(view: View) {
+        val cardPosition = view.id
+        val cardName = getCardName(cardPosition)
+        val textView = findViewById<TextView>(R.id.selectedCardTextView)
+        if (selectedCard == null) {
+            if (isEndDeckClick(resources.getResourceEntryName(cardPosition).split("subDeck")[1])) {
+                return
+            }
+            selectedCard = cardName
+            selectedPositionId = view.id
+            textView.text = CardNameTranslator.translate(cardName)
+        } else {
+            val desiredPosition = resources.getResourceEntryName(cardPosition).split("subDeck")[1]
+            if (canBeInserted(cardName, selectedCard!!, isEndDeckClick(desiredPosition))) {
+                moveCard(cardPosition, desiredPosition)
+            }
+
+            textView.text = resources.getString(R.string.nessuna_carta_selezionata)
+            selectedPositionId = null
+            selectedCard = null
+        }
+    }
+
+    private fun moveCard(cardPosition: Int, desiredPosition: String) {
+        setImage(cardPosition, selectedCard!!)
+        val selectedPositionName =
+            resources.getResourceEntryName(selectedPositionId!!).split("subDeck")[1]
+
+        cardTableMap[desiredPosition]?.add(selectedCard!!)
+        if (!isEndDeckClick(desiredPosition)) {
+            setNumberOfCards(cardTableMap[desiredPosition]!!, desiredPosition)
+        }
+
+        cardTableMap[selectedPositionName]!!.remove(selectedCard!!)
+        setNumberOfCards(cardTableMap[selectedPositionName]!!, selectedPositionName)
+
+        if (cardTableMap[selectedPositionName]!!.isEmpty()) {
+            setImage(selectedPositionId!!, "zero")
+        } else {
+            setImage(selectedPositionId!!, cardTableMap[selectedPositionName]!!.last())
+        }
+    }
+
+    private fun setNumberOfCards(cardsList: List<String>, position: String) {
+        val textView = findViewById<TextView>(getTextViewByName(position))
+        if (cardsList.size > 1) {
+            textView.text = cardsList.size.toString()
+        } else {
+            textView.text = ""
+        }
+    }
+
+    private fun isEndDeckClick(positionName: String) = positionName.last() == '4'
+
+    private fun canBeInserted(sourceCard: String, movingCard: String, endClickDeck: Boolean): Boolean {
+        val sourceSeme = sourceCard.substring(0, 1)
+        val movingSame = movingCard.substring(0, 1)
+        val movingNumber = movingCard.substring(1, movingCard.length).toInt()
+        if (endClickDeck ) {
+            if (sourceCard == "zero") {
+                return movingNumber == 1
+            }
+
+            val sourceNumber = sourceCard.substring(1, sourceCard.length).toInt()
+            if (sourceNumber == movingNumber - 1) {
+                return true
+            }
+        } else {
+            if (sourceSeme == movingSame) {
+                val sourceNumber = sourceCard.substring(1, sourceCard.length).toInt()
+                if (sourceNumber - 1 == movingNumber) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+
+    private fun getDrawableByName(imageName: String) =
+            resources.getIdentifier("drawable/$imageName", "id", this.packageName)
+
+    private fun getTextViewByName(textName: String) =
+        resources.getIdentifier("textView$textName", "id", this.packageName)
+
+    private fun prepareTable() {
+        arrayOf("1", "2", "3", "4").forEach { line ->
+            dealCard(line)
+        }
+    }
+
+    private fun getSubDeckListConcurrentSafely(line: String) : MutableList<String> {
+        var list = ArrayList<String>()
+        list.addAll(subDeckMap[line]!!)
+        return list
+    }
+
+    private fun dealCard(line: String) {
+        val subDeck = getSubDeckListConcurrentSafely(line)
+        val iterator = subDeck.iterator()
+        while (iterator.hasNext()) {
+            val cardName = iterator.next()
+            for (pos in 1..3) {
+                val position = "$line${pos}"
+                val imageViewId =
+                    resources.getIdentifier("subDeck$position", "id", this.packageName)
+                if (getCardName(imageViewId) == "zero") {
+                    setImage(imageViewId, cardName)
+                    cardTableMap[position] = arrayListOf(cardName)
+                    iterator.remove()
+                    break
+                }
+            }
+        }
+
+        subDeckMap[line] = subDeck
+    }
+
+    private fun setImage(position: Int, imageName: String) {
+        val imageView = findViewById<ImageView>(position)
+        val id = getDrawableByName(imageName)
+        imageView.setImageResource(id)
+        imageView.tag = imageName
+    }
+
+    private fun getCardName(cardPosition: Int): String {
+        val imageView = findViewById<ImageView>(cardPosition)
+        return (imageView.tag) as String
     }
 }
