8ec5eca737a2903790867226b90ce90e6f0d1278
Bruno Bottazzini <bottazzinibruno@gmail.com>
2026-05-19 18:21:25 +0200

    fix: correct Calendar.MONTH 0-indexing in dayKey helper

--- app/src/main/java/com/bottazzini/trasloco/utils/AchievementEngine.kt
+++ app/src/main/java/com/bottazzini/trasloco/utils/AchievementEngine.kt
@@ -154,7 +154,7 @@ class AchievementEngine(
             val cal = Calendar.getInstance().apply { timeInMillis = ts }
             return "%d-%02d-%02d".format(
                 cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
+                cal.get(Calendar.MONTH) + 1,
                 cal.get(Calendar.DAY_OF_MONTH)
             )
         }
